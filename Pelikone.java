2c13178927803b3aedea12ce755624ffd7900f6f
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-09 11:17:15 +0200

    Create Pelikone.java

---
+++ Pelikone.java
@@ -0,0 +1,15 @@
+//Peliautomaatti (Olio)
+  //Panos
+    //0.20 default
+  //Rahamäärä (double)
+  //Voitot (double)
+  
+//Set panos
+  //if 0.20 then 0.40
+  //if 0.40 then 0.60
+  //jne...
+  
+//Voitot rahamäärään metodi
+  
+//Tyhjennä voitot metodi
+  //Voitot lompakkoon

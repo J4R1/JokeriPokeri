872bb9ee56ab485308c1589b6d7727f123c8bf95
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-09 12:36:29 +0200

    Create Tuplaus.java

---
+++ Tuplaus.java
@@ -0,0 +1,17 @@
+//Tuplaaminen
+  //Kysy pelaajalta valinta [PIENI]/[SUURI]
+  //Käytä kortinnosto funktiota
+  //Tarkista käsi arraysta nostettu kortti
+    //Jokeri (automaattinen voitto)
+    //Pieni (1-6)
+    //Suuri (8-13)
+    //Musta seitsemän (häviö)
+        //Tuplaus päättyy
+    //Punainen seitsemän (rahat takaisin)
+        //Tuplaus päättyy
+  //Jos voitto
+      //Voitot 2x, Mahdollisuus uuteen tuplaamiseen
+          //[TUPLAUS] Jatka tuplausta
+          //[VOITONJAKO] Lisää voitot pelikoneen rahamäärään
+  //Jos häviö
+      //Voitot 0x, Tuplaus päättyy

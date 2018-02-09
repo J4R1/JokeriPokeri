0c6d9d04f32d9f7c8c48790fdb3f45f30879282a
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-09 12:23:55 +0200

    Create KorttiKasittely.java

---
+++ KorttiKasittely.java
@@ -0,0 +1,22 @@
+//Korttien käsittely (Funktiot)
+  
+  //Luo korttipakka array
+    //For kortinmaat (1-4)
+      //For (1-13)
+        //Luo kortti olio
+        //Vie kortti pakka arrayhin
+  //Lopuksi palauta pakka array
+  
+  //Korttien nosto funktio
+    //Ottaa vastaan käsi arrayn ja pakka arrayn
+    //Math.random valitse pakasta yksi kortti
+    //Vie kortti käsi arrayhin ja poista kortti pakka arraysta
+      
+  //Korttien siirtäminen muuhun kasaan
+    //Ottaa vastaan käsi arrayn ja muu arrayn
+    //Vie kortti muu arrayhin ja poista kortti käsi arraysta
+    
+  //Korttien sekoitus takaisin pakkaan
+      //Ottaa vastaan käsi arrayn ja muu arrayn
+      //Vie kortit pakka arrayhin ja poista kortti käsi ja muu arraysta
+  

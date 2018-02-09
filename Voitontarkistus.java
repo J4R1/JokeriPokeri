5742c59fda91a50c0452f41e08b02db5f72fcc7d
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-09 12:31:17 +0200

    Create Voitontarkistus.java

---
+++ Voitontarkistus.java
@@ -0,0 +1,15 @@
+//Voitontarkistus (Funktiot)
+  //Jokaiselle voitolle oma funktio 
+      //Funktio ottaa vastaan käsi arrayn ja palauttaa boolean true/false
+  //Voittomäärään laskemiselle funktio
+    //Ottaa vastaan panoksen ja voiton
+    //Voittokertoimet voi säilyttää constant muuttujissa
+      //Viitoset      50x
+      //Värisuora     30x
+      //Neloset       15x
+      //Täyskäsi      8x
+      //Väri          4x
+      //Suora         3x
+      //Kolmoset      2x
+      //Kaksi paria   2x
+      //Ei voittoa    0x

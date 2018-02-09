b1d7378d246600cf97809471400349d95fc485cc
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-09 11:28:48 +0200

    Create Kortti.java

---
+++ Kortti.java
@@ -0,0 +1,23 @@
+//Kortti (Olio)
+  //ID (Onko tarpeellinen?)
+      //1-53
+      //Juokseva luku korttien käsittelyä varten
+      //esim Math.random valitsemista varten
+      //Jokeri kortti 53 käyttäytyminen
+          //Loopataan voitontarkistusta katsoessa jokerille kaikki värit ja arvot 1-13
+  //Arvo
+      //1-13
+      //Arvon nimen voi hakea omalla funktiolla
+          //1 = A, 11 = J, 12 = Q, 13 = K
+  //Maa
+      //1-4
+      //Kortin maan voi hakea omalla funktiolla
+          //esim. 1 = hertta, 2 = ruutu, 3 = pata, 4 = risti
+      //Värin voi hakaea omalla funktiolla
+          //esim. 1-2 = punainen, 3-4 = musta
+      
+  //Getterit
+    //Kortin nimen hakeminen
+    //Kortin värin hakeminen
+    //Kortin maan hakeminen
+  

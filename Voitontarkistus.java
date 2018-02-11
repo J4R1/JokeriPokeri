b76669c4df4f543b5bb8f146d8d4030f02ad2412
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-11 21:47:33 +0200

    Update Voitontarkistus.java

--- Voitontarkistus.java
+++ Voitontarkistus.java
@@ -1,15 +1,51 @@
-//Voitontarkistus (Funktiot)
-  //Jokaiselle voitolle oma funktio 
-      //Funktio ottaa vastaan käsi arrayn ja palauttaa boolean true/false
-  //Voittomäärään laskemiselle funktio
-    //Ottaa vastaan panoksen ja voiton
-    //Voittokertoimet voi säilyttää constant muuttujissa
-      //Viitoset      50x
-      //Värisuora     30x
-      //Neloset       15x
-      //Täyskäsi      8x
-      //Väri          4x
-      //Suora         3x
-      //Kolmoset      2x
-      //Kaksi paria   2x
-      //Ei voittoa    0x
+package oliot_projekti;
+
+import java.util.ArrayList;
+
+public class Voitontarkistus {
+    
+    //Voittokertoimet myöhempää käyttöä varten
+    //UPPER CASE merkiksi siitä että arvot ovat lopullisia
+    final int KERROIN_VIITOSET = 50;
+    final int KERROIN_VARISUORA = 30;
+    final int KERROIN_NELOSET = 15;
+    final int KERROIN_TAYSKASI = 8;
+    final int KERROIN_VARI = 4;
+    final int KERROIN_SUORA = 3;
+    final int KERROIN_KOLMOSET = 2;
+    final int KERROIN_KAKSI_PARIA = 2;
+    final int KERROIN_EI_VOITTOA = 0;
+
+    //Ota vastaan käsikortit array, ja säilytettävien korttien array
+    public static void LukitseVoitot(ArrayList<Kortti> kortitKasi, Boolean[] bSailytaKortit) {
+        //Tässä voisi olla muuttujina esim.
+        //boolean suora, vari, tayskasi, kolmoset, neloset, viitoset;
+        //int pari;
+        
+        //Voittojen selvitykseen olisi ehkä helpointa siirtää käsikortit kasvavaan järjestykseen
+            //Varmaan kannattaisi tehdä kopio käsikorteista uuteen kortti arrayhin
+            //tällöin käsikorttien järjestys ei muutu
+        
+        //Esim. Selvitä kaikki booleanit ja parien määrät ja lopuksi valitse arvokkain voitto
+        
+        //Aseta bSailytaKortit arrayhin säilytettävät kortit
+        
+    }
+    
+    //Voitontarkistus (Funktiot)
+        //Jokaiselle voitolle oma funktio 
+            //Funktio ottaa vastaan käsi arrayn ja palauttaa boolean true/false
+    //Voittomäärään laskemiselle funktio
+        //Ottaa vastaan panoksen ja voiton
+        //Voittokertoimet voi säilyttää constant muuttujissa
+            //Viitoset      50x
+            //Värisuora     30x
+            //Neloset       15x
+            //Täyskäsi      8x
+            //Väri          4x
+            //Suora         3x
+            //Kolmoset      2x
+            //Kaksi paria   2x
+            //Ei voittoa    0x
+
+}

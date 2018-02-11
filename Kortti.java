f7845e17535ef2732d039c77dfe5d88a7b4bc3f6
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-11 17:52:45 +0200

    Update Kortti.java

--- Kortti.java
+++ Kortti.java
@@ -5,24 +5,12 @@ public class Kortti {
     int ID, iKortinArvo, iKortinMaa;
     boolean bJokeri = false; //Onko tarpeellinen?
     
-  //ID (Onko tarpeellinen?)
-        //1-53
-        //Juokseva luku korttien käsittelyä varten
-        //esim Math.random valitsemista varten
-        //Jokeri kortti 53 käyttäytyminen
-            //Loopataan voitontarkistusta katsoessa jokerille kaikki värit ja arvot 1-13
-    //Arvo
-        //1-13
-        //Arvon nimen voi hakea omalla funktiolla
-            //1 = A, 11 = J, 12 = Q, 13 = K
-    //Maa
-        //1-4
-        //Kortin maan voi hakea omalla funktiolla
-            //esim. 1 = hertta, 2 = ruutu, 3 = pata, 4 = risti
-        //Värin voi hakaea omalla funktiolla
-            //esim. 1-2 = punainen, 3-4 = musta
+    public String toString() {
+        //Metodin kannattaa nyt palauttaa merkkijono: etunimi + ” ” + sukunimi
+        return this.getKorttiMaa() + " " + this.getKorttiNimi();
+    }
     
-  //Kortin luominen
+    //Tavallisen kortin luominen
     public Kortti(int iIndex, int iArvo, int iMaa, boolean bOnkoJokeri) {
         this.ID = iIndex;
         this.iKortinArvo = iArvo;
@@ -57,6 +45,8 @@ public class Kortti {
     
     public String getKorttiMaa() {
         switch (this.iKortinMaa) {
+            case 0:
+                return "Joker";
             case 1:
                 return "Hertta";
             case 2:
@@ -69,7 +59,9 @@ public class Kortti {
     }
     
     public String getKorttiVari() {
-        if (this.iKortinMaa == 1 || this.iKortinMaa == 2) {
+        if (this.iKortinMaa == 0) {
+            return "Joker";
+        } else if (this.iKortinMaa == 1 || this.iKortinMaa == 2) {
             return "Punainen";
         } else {
             //Jätkä

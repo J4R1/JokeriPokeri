f22d0d834bbd55a63f41fb79c2e568db2da2169e
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-12 23:16:28 +0200

    Update JokeriPokeriTest.java

--- JokeriPokeriTest.java
+++ JokeriPokeriTest.java
@@ -4,48 +4,31 @@ import java.util.ArrayList;
 
 public class JokeriPokeriTest {
     public static void main(String[] args) {
-        //Korttien käsittely (Funktiot)
+        
         ArrayList<Kortti> kortitPakka = new ArrayList<Kortti>();
         ArrayList<Kortti> kortitKasi = new ArrayList<Kortti>();
         ArrayList<Kortti> kortitMuu = new ArrayList<Kortti>();
         
-        //Kortti[] kortitPakka = new Kortti[53]; 
-        //Kortti[] kortitKasi = new Kortti[0];
-        //Kortti[] kortitMuu = new Kortti[0];
-        
-        int iKorttiIndex = 0;
-        
-        //Luo kortti oliot pakka arrayhin
-        //For kortin maat (1-4)
-        for (int iIndexMaat = 1; iIndexMaat <= 4; iIndexMaat++) {
-            //For kortin arvot (1-13)
-            for (int iIndexArvo = 1; iIndexArvo <= 13; iIndexArvo++) {
-                //Luodaan 52 peruskortti oliota suoraan korttipakka arrayhin
-                kortitPakka.add(iKorttiIndex, new Kortti(iKorttiIndex, iIndexArvo, iIndexMaat, false));
-                iKorttiIndex++;
-            }
-        }
-        //Luodaan jokeri kortti
-        kortitPakka.add(iKorttiIndex, new Kortti(iKorttiIndex, 0, 0, true));
+        //Alusta kaikki kortit ja siirrä ne korttipakkaan
+        KorttiKasittely.LuoKorttipakka(kortitPakka);
         
         //Nostetaan viisi käsikorttia
         while (kortitKasi.size() < 5) {
-            //Siirretään satunnainen kortti käsikortteihin
-            int iNostettavaKortti;
-            //Valitaan pakasta satunnainen indeksi
-            iNostettavaKortti = (int) (Math.random() * kortitPakka.size());
-            //Näytä nostettu kortti
-            System.out.println("Nostit kortin " + kortitPakka.get(iNostettavaKortti));
-            //Siirrä nostettu kortti käsikortteihin
-            kortitKasi.add(kortitPakka.get(iNostettavaKortti));
-            //Poista kortti pakasta
-            kortitPakka.remove(iNostettavaKortti);
+            //Kutsu kortinnosto komentoa
+            KorttiKasittely.NostaKortti(kortitPakka, kortitKasi);
         }
         
         //Tähän voitontarkistus
+        //esim. 
+        //Voitontarkistus.TarkistaJaLukitseVoitot(kortitKasi);
+            //Ota selvää millä korteilla saa voiton
+                //Lukitse voittavat kortit
+                //Visuaalinen merkki siitä mikä voitto varmistui
         
         //Kysy säilytettävät kortit
         //Tähän jokin systeemi korttien valitsemiseen
+        
+        //[JAKO] painiketta painettu
         System.out.println("");
         System.out.println("Valitsit vaihdettaviksi korteiksi:");
         System.out.println(kortitKasi.get(0));
@@ -59,76 +42,33 @@ public class JokeriPokeriTest {
         kortitKasi.remove(1);
         kortitMuu.add(kortitKasi.get(0));
         kortitKasi.remove(0);
+        //Tähän tulisi oikeasti esim.
+        //KorttiKasittely.SiirraKortitMuualle(kortitKasi, bSailytaKortit)
         
         //Nosta käsikortteihin kunnes kädessä on viisi korttia
         while (kortitKasi.size() < 5) {
-            //Siirretään satunnainen kortti käsikortteihin
-            int iNostettavaKortti;
-            //Valitaan pakasta satunnainen indeksi
-            iNostettavaKortti = (int) (Math.random() * kortitPakka.size());
-            //Näytä nostettu kortti
-            System.out.println("Nostit kortin " + kortitPakka.get(iNostettavaKortti));
-            //Siirrä nostettu kortti käsikortteihin
-            kortitKasi.add(kortitPakka.get(iNostettavaKortti));
-            //Poista kortti pakasta
-            kortitPakka.remove(iNostettavaKortti);
+            //Ilmoita funktiolle mihin positioon nostettu kortti laitetaan
+            //Tämä sen vuoksi että jäljelle jäävät kortit eivät siirry
+            KorttiKasittely.NostaKortti(kortitPakka, kortitKasi);
         }
         
         //Tähän voitontarkistus
         
-        //TEST, tulostetaan kaikki kortit pakassa
-        System.out.println("");
-        System.out.println("Korttipakassa on " + kortitPakka.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitPakka) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
-        //TEST, tulostetaan kaikki kortit kädessä
-        System.out.println("");
-        System.out.println("Käsikorteissa on " + kortitKasi.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitKasi) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
-        //TEST, tulostetaan kaikki kortit kädessä
-        System.out.println("");
-        System.out.println("Muissa korteissa on " + kortitMuu.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitMuu) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
+        //TEST, tulostetaan kaikki kortit arrayt
+        KorttiKasittely.TulostaKortit(kortitPakka, "Korttipakassa");
+        KorttiKasittely.TulostaKortit(kortitKasi, "Kädessä");
+        KorttiKasittely.TulostaKortit(kortitMuu, "Muissa korteissa");
         
         System.out.println("");
         System.out.println("Sekoitetaan kortit takaisin pakkaan");
         
         //Sekoitetaan kortit takaisin pakkaan
-        while (kortitKasi.size() > 0) {
-            //Siirrä nostettu kortti käsikortteihin
-            kortitPakka.add(kortitKasi.get(0));
-            //Poista kortti pakasta
-            kortitKasi.remove(0);
-        }
-        while (kortitMuu.size() > 0) {
-            //Siirrä nostettu kortti käsikortteihin
-            kortitPakka.add(kortitMuu.get(0));
-            //Poista kortti pakasta
-            kortitMuu.remove(0);
-        }
+        KorttiKasittely.SiirraKortitToiseenKasaan(kortitPakka, kortitKasi);
+        KorttiKasittely.SiirraKortitToiseenKasaan(kortitPakka, kortitMuu);
         
-        //TEST, tulostetaan kaikki kortit pakassa
-        System.out.println("");
-        System.out.println("Korttipakassa on " + kortitPakka.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitPakka) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
-        //TEST, tulostetaan kaikki kortit kädessä
-        System.out.println("");
-        System.out.println("Käsikorteissa on " + kortitKasi.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitKasi) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
-        //TEST, tulostetaan kaikki kortit kädessä
-        System.out.println("");
-        System.out.println("Muissa korteissa on " + kortitMuu.size() + " korttia, jotka ovat:");
-        for (Kortti kortti : kortitMuu) {
-            System.out.println(kortti.getKorttiMaa() + " " + kortti.getKorttiNimi() + " " + kortti.getKorttiVari());
-        }
+        //Tulostetaan kortit arrayt
+        KorttiKasittely.TulostaKortit(kortitPakka, "Korttipakassa");
+        KorttiKasittely.TulostaKortit(kortitKasi, "Kädessä");
+        KorttiKasittely.TulostaKortit(kortitMuu, "Muissa korteissa");
     }
 }

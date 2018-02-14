0da44ca8161416720a86747f9a5436745a6fdb34
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-14 11:36:16 +0200

    Update Voitontarkistus.java

--- Voitontarkistus.java
+++ Voitontarkistus.java
@@ -36,9 +36,11 @@ public class Voitontarkistus {
             //tällöin käsikorttien järjestys ei muutu
             //ks. Multidimensional array
                 //int[][] aryNumbers = new int[5][3]; //Viisi riviä, 3 saraketta
-                //aryNumbers[0][0] //Esim. Ensimmäisen kortin positio
+                //aryNumbers[0][0] //Esim. Ensimmäisen kortin positio kädessä
                 //aryNumbers[0][1] //Esim. Ensimmäisen kortin arvo
                 //aryNumbers[0][2] //Esim. Ensimmäisen kortin maa
+                //aryNumbers[1][0] //Esim. Toisen kortin positio kädessä
+                //aryNumbers[1][1] //Esim. Toisen kortin arvo jne...
                 
         //Esim. Selvitä kaikki booleanit ja parien määrät ja lopuksi valitse arvokkain voitto
         

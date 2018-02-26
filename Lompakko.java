b6e303e9bba4686b74b42ddbe6420fc1ec4e8296
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-02-26 10:48:09 +0200

    Update Lompakko.java

--- Lompakko.java
+++ Lompakko.java
@@ -1,10 +1,23 @@
+package oliot_projekti;
+
 //Lompakko (Olio)
-  //Rahamäärä (double)
-      //Valinnainen vaihtoehto: 
-      //0.20 kolikkomäärä (int)
-      //0.50 kolikkomäärä (int)
-      //1.0 kolikkomäärä (int)
-  
-  //Rahamäärä getteri
-  
-  //Vähennä metodi
+public class Lompakko {
+    //Rahamäärä (double)
+    double dRahamaara = 0;
+        //Valinnainen vaihtoehto: 
+        //0.20 kolikkomäärä (int)
+        //0.50 kolikkomäärä (int)
+        //1.0 kolikkomäärä (int)
+    //Rahamäärä getteri
+    public double getdRahamaara() {
+        return dRahamaara;
+    }
+    //Lisää metodi
+    public void LisaaRahamaara(double dLisattavaMaara) {
+        this.dRahamaara = this.dRahamaara + dLisattavaMaara;
+    }
+    //Vähennä metodi
+    public void VahennaRahamaara(double dVahennettavaMaara) {
+        this.dRahamaara = this.dRahamaara - dVahennettavaMaara;
+    }
+}

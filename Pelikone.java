7309a727966cc44c55cba8e2a47a5af48025ad68
Habatsuki <36292528+Habatsuki@users.noreply.github.com>
2018-03-06 10:28:35 +0200

    Update Pelikone.java

--- Pelikone.java
+++ Pelikone.java
@@ -2,17 +2,10 @@ package oliot_projekti;
 
 //Peliautomaatti (Olio)
 public class Pelikone {
-        //Panos
         double dPanos = 0.20;
         double dMinPanos = 0.20, dMaxPanos = 1.0;
-            //0.20 default
-        //Rahamäärä (double)
         double dRahamaara = 0;
-            //Default 0
-                //Valinnainen: Edellinen käyttäjä jättänyt rahaa Math.random
-        //Voitot (double)
         double dVoitot = 0;
-            //Default 0
         int iPelivaihe = 1;
         
         public Pelikone() {
@@ -25,6 +18,8 @@ public class Pelikone {
             if (this.dPanos > dMaxPanos) {
                 this.dPanos = dMinPanos;
             }
+            //Kirjoita uusi panos
+            Gui_Main.jLab_18_PanosSumma.setText(String.valueOf(this.dPanos));
         }
         //Voitot rahamäärään metodi
         public void AsetaVoitotRahamaara(double dAsetettavaVoitto) {
@@ -37,6 +32,7 @@ public class Pelikone {
                 this.dRahamaara = this.dRahamaara + this.dVoitot;
                 this.dVoitot = 0;
             }
+            //Päivitä voitot ja rahamäärät labeleihin
         }
         //Voitot lompakkoon
         public void VoitotLompakkoon(Lompakko targetLompakko) {
@@ -47,7 +43,25 @@ public class Pelikone {
         public int getiPelivaihe() {
             return iPelivaihe;
         }
+        public double getdPanos() {
+            return dPanos;
+        }
         public void setiPelivaihe(int iAsetaVaihe) {
             this.iPelivaihe = iAsetaVaihe;
+
+            //Aseta painikkeet lukituiksi
+            if (this.iPelivaihe == 1) {
+                //Esim:
+                //Gui_Main.jB2_Tuplaa.setEnabled(false);
+                //Gui_Main.jB4_Jaa.setEnabled(true);
+            } else if (this.iPelivaihe == 2) {
+
+            } else if (this.iPelivaihe == 3) {
+
+            } else if (this.iPelivaihe == 4) {
+
+            } else {
+
+            }
         }
 }
